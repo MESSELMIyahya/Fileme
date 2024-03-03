9b7c593b4170f156bbea10001b3d99fe130f206f
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-03-03 23:29:36 +0100

    Change some styles in the Header and Add it to the Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Header from "@/components/Header";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -20,8 +21,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("min-h-screen bg-background font-sans antialiased",fontSans.variable)} 
-      >{children}</body>
+
+      <body className={cn("min-h-screen bg-background font-sans antialiased",fontSans.variable)}>
+
+        <Header/>
+        {children}
+      
+      </body>
     </html>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,10 +8,10 @@ import { Button } from "../ui/button";
 export default function Header (){
 
 
-    return (<header className="container flex items-center justify-between  py-5 bg-background ">
+    return (<header className="container flex items-center justify-between  py-4 bg-background ">
         
             <div className="">
-                    <Link href='/' className="text-4xl font-semibold text-foreground">
+                    <Link href='/' className="text-4xl font-bold text-foreground">
                             File
                             <span className="text-primary">Me</span>
                     </Link>
@@ -20,9 +20,9 @@ export default function Header (){
             <nav className="flex gap-8 items-center">
 
                 <div className="flex items-center gap-4">
-                        <Link href='/' className="text-foreground/80 hover:underline">Home</Link>
-                        <Link href='/' className="text-foreground/80 hover:underline">Pricing</Link>
-                        <Link href='/' className="text-foreground/80 hover:underline">Contact</Link>
+                        <Link href='/' className="text-foreground/80 transition-colors hover:text-primary">Home</Link>
+                        <Link href='/' className="text-foreground/80 transition-colors hover:text-primary">Pricing</Link>
+                        <Link href='/' className="text-foreground/80 transition-colors hover:text-primary">Contact</Link>
                 </div>
 
                 <div className="flex items-center gap-3">

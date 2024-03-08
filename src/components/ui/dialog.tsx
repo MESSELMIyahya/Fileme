0a774ba84c14a173624ce157a0ea951dd674ee17
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-03-08 14:22:49 +0100

    Add Sheet Component and Dialog Component

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
-import { X } from "lucide-react"
+import { HiX as X } from "react-icons/hi"
 
 import { cn } from "@/lib/utils"
 

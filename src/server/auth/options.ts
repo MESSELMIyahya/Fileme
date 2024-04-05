1860909f9fd43e3b7cf328c9f1de967572c4e727
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-04-05 02:02:56 +0100

    Remove GithubProvider and Add Credentials login with all verifying system and  checking the user in db just like login user in google provider

--- src/server/auth/options.ts
+++ src/server/auth/options.ts
@@ -1,5 +1,6 @@
 import { AuthOptions } from 'next-auth'
-import GithubProvider from 'next-auth/providers/github';
+// import GithubProvider, { GithubProfile } from 'next-auth/providers/github';
+import CredentialsProvider, { CredentialInput } from 'next-auth/providers/credentials'
 import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google';
 import { connectToDB } from '../db';
 import { UserSchemaType } from '../db/models/User/type';
@@ -9,15 +10,17 @@ import { calcSize, freeStorageSize } from '@/lib/storage';
 
 const authOptions: AuthOptions = {
     providers: [
+        // google
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 
+            //@ts-ignore
             async profile(profile, tokens) {
 
                 try {
 
-                    const { email,...googleUser }: GoogleProfile = profile
+                    const { email, ...googleUser }: GoogleProfile = profile
 
                     // connect to the db
 
@@ -53,9 +56,9 @@ const authOptions: AuthOptions = {
                                 provider: 'google'
                             },
                             email,
-                            firstName:googleUser.given_name,
-                            lastName:googleUser.family_name,
-                            username:googleUser.name,
+                            firstName: googleUser.given_name,
+                            lastName: googleUser.family_name,
+                            username: googleUser.name,
                             pic: googleUser.picture,
                         },
                         storage: {
@@ -76,7 +79,7 @@ const authOptions: AuthOptions = {
 
                     // save the user 
 
-                    const user =  await newUser.save();
+                    const user = await newUser.save();
 
                     // return the user 
                     return {
@@ -90,14 +93,61 @@ const authOptions: AuthOptions = {
                 }
             }
         }),
-        GithubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID as string,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
 
-            async profile(profile, tokens) {
-                return { ...profile }
+
+        // credentials 
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {},
+            async authorize(c) {
+                try {
+
+                    const { email, password } : { email: string, password: string } = c as never;
+
+                    // connect to the db
+
+                    await connectToDB();
+
+                    // verifying if the user exists
+                    // @ts-ignore
+                    const existedUser: boolean = await UserModel.DoesUserExists(email);
+
+                    // if user doesn't exists
+                    if (!existedUser) {
+                        return null;
+                    }
+
+                    // fetch the user 
+                    const user = await UserModel.findOne({ 'account.email': email });
+
+                     // if user doesn't exists
+                    if (!user) {
+                        return null;
+                    }
+
+                    // compare passwords 
+                    // @ts-ignore
+                    const isValidPass : boolean = await user.ComparePasswords(password); 
+
+                    // if the passwords aren't match
+                    if(!isValidPass){
+                        return  null;
+                    }
+
+                    // return the user 
+                    return {
+                        id: user?._id,
+                        email: user?.account.email,
+                        image: user?.account.pic,
+                        name: user?.account.username
+                    } as never
+                } catch (err) {
+                    return null;
+                }
             }
+
         })
+
     ],
 
     pages: {

f57837aef337d4057e9d880dd02cae88b3703585
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-04-09 00:10:12 +0100

    Add scroll bar just for the pages inside the dahsboard and remove it from the dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,10 +6,10 @@ import SideBar from "./_components/Sidebar";
 import NavBar from "./_components/Navbar";
 
 interface Props {
-    children:React.ReactNode   
+    children: React.ReactNode
 }
 
-export default async function DashboardRootLayout ({ children }:Readonly<Props>){
+export default async function DashboardRootLayout({ children }: Readonly<Props>) {
     // const au = await getServerSession();
 
     // if(!au?.user){
@@ -17,19 +17,19 @@ export default async function DashboardRootLayout ({ children }:Readonly<Props>)
     //     return redirect('/')
     // }
 
-    return (<div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    return (<div className="grid min-h-screen h-screen overflow-hidden w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
         {/* sidebar */}
 
         <SideBar selected="home" />
 
         {/* page */}
 
-        <div className="flex flex-col">
-            <NavBar/>
-            <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-                {children}
-            </main>
+        <div className="w-full min-h-screen flex flex-col">
+            <NavBar />
+                 <div className="flex flex-1 h-full overflow-hidden overflow-y-scroll flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+                     {children}
+                 </div>
         </div>
-    
+
     </div>)
 }
